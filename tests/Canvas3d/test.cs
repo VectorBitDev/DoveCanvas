ed5da37b2b4c31a2227244004ff2cc69c4af54e0
Samuel <samuelsmendes2008@gmail.com>
2026-07-29 14:30:29 +0100

    Bug Fixes!
    Map Service Implemented
    Better Free Camera Movement
    Fixed Resource Service paths
    Fixed Rendering Systems, and added DebugRendering
    Added Canvas 3d Test

---
+++ tests/Canvas3d/test.cs
@@ -0,0 +1,14 @@
+using DoveCanvas.Abstract;
+using DoveCanvas;
+
+public class TestScene : Scene
+{
+    public override void Load()
+    {
+    }
+
+    public override void Unload()
+    {
+    }
+
+}
